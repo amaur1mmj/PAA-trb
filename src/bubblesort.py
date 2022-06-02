38dd4397c42049e6e07554d385a2a3c1bb4b9b76
amaur1mmj@gmail.com <amaur1mmj@gmail.com>
2022-06-01 21:13:15 -0300

    ok

--- src/bubblesort.py
+++ src/bubblesort.py
@@ -6,21 +6,23 @@ t1 = time.time()
 
 
 def bubbleSort(alist):
+    troca = 0
     for passnum in range(len(alist)-1, 0, -1):
         for i in range(passnum):
             if alist[i] > alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
+                troca += 1
+    return troca
 
 
 alist = fileReader.file_read("cem.txt")
-print(alist)
-print("\n")
 
-bubbleSort(alist)
+a1 = bubbleSort(alist)
 
 tf = time.time() - t1
 print(alist)
 print("\n")
+print(a1)
 print(tf)
